e4342f44ae60c35b0a6878ba29afcc4c4cbf4583
UiChan Lee <162671129+ana-raka@users.noreply.github.com>
2024-03-28 14:36:47 +0900

    [Silver III] Title: 수강신청, Time: 484 ms, Memory: 74580 KB -BaekjoonHub

--- 백준/Silver/13414. 수강신청/수강신청.py
+++ 백준/Silver/13414. 수강신청/수강신청.py
@@ -1,20 +1,15 @@
 import sys
 input = sys.stdin.readline
-from collections import OrderedDict
 
 n, m = map(int,input().split())
 dic = {}
 for i in range(m):
     stdid = input().rstrip()
-    try:
-        dic[stdid][1] = i
-    except:
-        dic[stdid] = [1,i]
+    dic[stdid] = i
 cnt = 0
-stdlist = sorted(dic.items(), key= lambda x : x[1][1])
+stdlist = sorted(dic.items(), key= lambda x : x[1])
 for stdid, val in stdlist:
-    if val[0] == 1:
-        print(stdid)
-        cnt += 1
+    print(stdid)
+    cnt += 1
     if cnt == n:
         exit()
