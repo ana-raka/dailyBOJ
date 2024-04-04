23c6251e47993e84255a95cfe6261ac2b79e1e4b
UiChan Lee <162671129+ana-raka@users.noreply.github.com>
2024-04-04 16:15:45 +0900

    [Gold I] Title: 택배, Time: 3568 ms, Memory: 37964 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/8980. 택배/택배.py
+++ 백준/Gold/8980. 택배/택배.py
@@ -2,45 +2,43 @@ import sys
 input = sys.stdin.readline
 from queue import PriorityQueue
 
-def load():
+def load(vil_num_load):
     global weight
-    boxl.sort(key=lambda x: (x[0], x[1], -x[2]))
-    while boxl and vil_num == boxl[0][0]:
-        f_vil, t_vil, val = boxl.pop(0)
+    boxl[vil_num_load].sort(key=lambda x: (x[0], x[1]))
+    for to_vil, val in boxl[vil_num_load]:
         if weight + val <= C:
-            car[t_vil].append(val)
+            car[to_vil].append(val)
             weight += val
         else:
             if weight == C:
-                continue
-            car[t_vil].append(C-weight)
+                break
+            car[to_vil].append(C-weight)
             weight = C
-
+        boxl[vil_num_load] = []
 def drop(vil_num_drop):
     global res, weight
-    for i in range(1,len(car)):
+    for i in range(vil_num_drop,len(car)):
         for val in car[i]:
             if i == vil_num_drop:
                 res += val
                 #weight -= val
             else:
-                boxl.append([vil_num_drop,i,val])
+                boxl[vil_num_drop].append([i,val])
         car[i] = []
     weight = 0
 
-
-
 N, C = map(int,input().split())
-boxl = []
+boxl = [[] for _ in range(N+1)]
 car = [[] for _ in range(N+1)]
 weight = 0
 res = 0
 for _ in range(int(input().rstrip())):
-    boxl.append(list(map(int,input().split())))
+    f_vil, t_vil, val = map(int,input().split())
+    boxl[f_vil].append([t_vil,val])
 for vil_num in range(1,N+1):
     if vil_num == 1:
-        load()
+        load(vil_num)
         continue
     drop(vil_num)
-    load()
+    load(vil_num)
 print(res)
