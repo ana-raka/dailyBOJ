41cb6bb266e350fe5734398d7118b357d5408654
UiChan Lee <162671129+ana-raka@users.noreply.github.com>
2024-04-16 22:08:01 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 4932 ms, Memory: 190972 KB -BaekjoonHub

---
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.py
@@ -0,0 +1,35 @@
+import sys
+input = sys.stdin.readline
+from collections import deque
+def bfs():
+    global res
+    while q:
+        y, x, w  = q.popleft()
+        if x == m-1 and y == n-1:
+            print(visit[y][x][w])
+            exit()
+        for i in range(4):
+            mvx = x + mx[i]
+            mvy = y + my[i]
+            if 0<= mvx <= m-1 and 0<= mvy <= n -1:
+                if visit[mvy][mvx][w] == 0 and mage[mvy][mvx] == 0:
+                    q.append((mvy, mvx, w))
+                    visit[mvy][mvx][w] = visit[y][x][w] + 1
+                elif mage[mvy][mvx] == 1 and w == 0:
+                    q.append((mvy, mvx, w + 1))
+                    visit[mvy][mvx][w + 1] = visit[y][x][w] + 1
+
+n,m = map(int,input().split())
+res = 1000001
+mx = [-1,1,0,0]
+my = [0,0,-1,1]
+mage = []
+for _ in range(n):
+    mage.append(list(map(int,input().rstrip())))
+
+visit = [[[0,0] for i in range(m)] for j in range(n)]
+q = deque()
+q.append((0, 0, 0))
+visit[0][0][0] = 1
+bfs()
+print(-1)
