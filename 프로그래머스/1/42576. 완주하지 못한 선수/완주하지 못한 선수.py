938502ff786e13876412efb0e61d7becf0626ebf
UiChan Lee <162671129+ana-raka@users.noreply.github.com>
2025-01-07 10:33:53 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 64.85 ms, Memory: 38.9 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,14 +1,5 @@
-# 알고리즘: 해시
-# 시간 복잡도:
-# 자료구조: 해시
+import collections
 
 def solution(participant, completion):
-    participant.sort()
-    completion.sort()
-    for i in range(len(completion)):
-        if participant[i] != completion[i]:
-            return participant[i]
-    else:
-        return participant[-1]
-    
-            
+    answer = collections.Counter(participant) - collections.Counter(completion)
+    return list(answer.keys())[0]
