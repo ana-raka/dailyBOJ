0f266bffda7e2ffefd17609ba1d36c882b0425d8
UiChan Lee <162671129+ana-raka@users.noreply.github.com>
2025-01-08 11:54:25 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 65.44 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.py
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.py
@@ -12,11 +12,11 @@ def solution(bridge_length, weight, truck_weights):
         if bridge and time - bridge[0][1] == bridge_length:
             now_weight -= bridge[0][0]
             bridge.popleft()
-        front_truck = truck_list[0]
-        if len(bridge) < bridge_length and (now_weight + front_truck) <= weight:
-            bridge.append((truck_list[0], time))
-            now_weight += truck_list[0]
-            truck_list.popleft()
             
+        if len(bridge) < bridge_length and (now_weight + truck_list[0]) <= weight:
+            waiting_truck = truck_list.popleft()
+            bridge.append((waiting_truck, time))
+            now_weight += waiting_truck
+        
         time += 1
     return time + bridge_length
