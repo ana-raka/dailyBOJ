6329c4d047a0d5a8d34f6c5232d1170e45174cff
UiChan Lee <162671129+ana-raka@users.noreply.github.com>
2025-01-08 09:27:17 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 65.43 ms, Memory: 32.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
@@ -0,0 +1,13 @@
+# 알고리즘: 
+# 시간복잡도:
+# 자료구조: 큐
+from collections import deque
+
+def solution(arr):
+    que = deque(arr)
+    answer = [-1]
+    for number in que:
+        if number != answer[-1]:
+            answer.append(number)
+    answer.pop(0)
+    return answer
