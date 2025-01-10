446863f8a434b14e7ee08d1ee5465e7b72f0996b
UiChan Lee <162671129+ana-raka@users.noreply.github.com>
2025-01-10 12:13:01 +0900

    [level 1] Title: 모의고사, Time: 7.36 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -1,20 +1,19 @@
 # 알고리즘: 완전탐색
 # 시간복잡도: O(N)
 # 자료구조: 리스트
-def is_answer(answer, student, idx):
-    if answer == student[idx % len(student)]:
-        return 1
-    return 0
+def is_correct(answer, student, idx):
+    return answer == student[idx % len(student)]
 
 def solution(answers):
-    s1 = [1,2,3,4,5]
-    s2 = [2,1,2,3,2,4,2,5]
-    s3 = [3,3,1,1,2,2,4,4,5,5]
+    patterns = [[1,2,3,4,5],
+                [2,1,2,3,2,4,2,5],
+                [3,3,1,1,2,2,4,4,5,5]
+               ]
     result_cnt = [0] * 3
     for idx , answer_num in enumerate(answers):
-        result_cnt[0] += is_answer(answer_num, s1, idx)
-        result_cnt[1] += is_answer(answer_num, s2, idx)
-        result_cnt[2] += is_answer(answer_num, s3, idx)
+        for i in range(3):
+            if is_correct(answer_num, patterns[i], idx):
+                result_cnt[i] += 1
     max_score = max(result_cnt)
     answer = [i + 1 for i in range(3) if result_cnt[i] == max_score]
     return answer
